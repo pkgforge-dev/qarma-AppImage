eb59dbe1d646d7e1e41b7667600fdd2078e14371
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-14 17:18:42 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,6 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package https://github.com/luebking/qarma
 
 # If the application needs to be manually built that has to be done down here
