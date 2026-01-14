155cc37bc8e7b3e71793637d44da357f15ea9246
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-14 17:35:54 -0400

    not needed

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q qarma | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=DUMMY
 export DESKTOP=DUMMY
